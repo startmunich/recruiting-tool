37f6ae4cd7899a1687c3c5d8de1962106e5cbc09
Arian Gohari <a.gohari@startmunich.de>
2024-04-19 22:22:13 +0200

    implemented airtable connection

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from pyairtable import Api
 import streamlit as st
 
+# load dotenv file
+load_dotenv(override=True)
 
-st.title('Hello World')
-st.write('This is a simple example of a Streamlit app.')
+# constants
+AIRTABLE_API_KEY = os.environ['AIRTABLE_API_KEY']
+AIRTABLE_BASE_ID = os.environ['AIRTABLE_BASE_ID']
+AIRTABLE_TABLE_ID = os.environ['AIRTABLE_TABLE_ID']
+
+# load records from airtable using pyairtable
+api = Api(AIRTABLE_API_KEY)
+table = api.table(AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID)
+record_list = table.all()
+
+# load data into pandas dataframe
+df = pd.DataFrame([record['fields'] for record in record_list], index=[record['fields']['Submission ID'] for record in record_list])
+
+# basic streamlit app
+st.title('Recruiting Tool')
+st.dataframe(df)
