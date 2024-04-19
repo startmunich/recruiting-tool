3632700a12a82578d45084111ffc08ab645bb3e9
Arian Gohari <a.gohari@startmunich.de>
2024-04-20 00:51:17 +0200

    merged

--- main.py
+++ main.py
@@ -27,15 +27,29 @@ df = pd.DataFrame([record['fields'] for record in record_list],
 # drop unnecessary columns
 df = df.drop(columns=["Index", "Respondant ID", "Submitted at"])
 
-
 # load css and apply to streamlit
 # css = open('static/css/style.css')
 # st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
+# init questions
+questions = df.columns[QUESTION_START:QUESTION_START + QUESTION_NUMBER].tolist()
+
+
 # basic streamlit app
 def render_page(row, completed):
     st.title(f'{completed} {row["First Name"]} {row["Last Name"]}')
-    st.dataframe(df)
+
+    st.header("Questions")
+    st.markdown("""---""")
+
+    for question in questions:
+        st.write(question)
+        st.write(row[question])
+        st.markdown("""---""")
+
+    st.header("Ranking")
+    st.write("Please give us a qualitative ranking based on the presented information about this candidate.")
+    st.text_area("")
 
 
 ids = df['Submission ID'].tolist()
@@ -46,18 +60,3 @@ titles = [f'{id} {completed}' for id, completed in zip(ids, completed)]
 
 selected_page, completed = st.sidebar.selectbox("Select an application", titles).split(" ")
 render_page(df.loc[selected_page], completed)
-
-questions = df.columns[QUESTION_START:QUESTION_START+QUESTION_NUMBER]
-user_index = 5
-
-st.header("Questions")
-st.markdown("""---""")
-
-for i in range(0, len(questions)):
-    st.write(questions[i])
-    st.write(df[(questions[i])][user_index])
-    st.markdown("""---""")
-
-st.header("Ranking")
-st.write("Please give us a qualitative ranking based on the presented information about this candidate.")
-st.text_area("")
