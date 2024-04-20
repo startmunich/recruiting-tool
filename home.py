ff0b12935a400b06df9471dd8217a4a190489928
NoelNikolai <nikolai.noel.mueller@gmail.com>
2024-04-20 02:51:53 +0200

    home update

--- home.py
+++ home.py
@@ -11,11 +11,10 @@ st.title(f"Hello {user}!")
 
 col1, col2 = st.columns([3, 2])
 with col1:
-    st.write("This is a new tool with which will help you slice through the application reviewing process easily. Choose from the applications on the left and START the journey!")
-    st.write("\n")
-    st.markdown(f"**You have {applications_left} applications left to review. Let's go 💪🏻!**")
+    st.write("This is a new tool which will help you slice through the application reviewing process easily. Choose from the applications on the left and may the onbaording START!")
+    st.subheader(f"You have {applications_left} applications left to review. Let's go 💪🏻!")
     if applications_reviewed != 0:
-        st.markdown(f"**You have already reviewed {applications_reviewed} 🥳**")
+        st.subheader(f"You have already reviewed {applications_reviewed} 🥳")
 
 with col2:
     st.image("start_polygon.png", width=250)
