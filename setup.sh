809c335d57f92f8a52b957fe20af3f832fb0514c
Arian Gohari <a.gohari@startmunich.de>
2024-04-21 23:13:30 +0200

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 mkdir -p ~/.streamlit/
-echo "\
-[theme]\n\
-primaryColor="#CA1551"\n\
-backgroundColor="#00002C"\n\
-secondaryBackgroundColor="#011152"\n\
-textColor="#FFFFFF"\n\
-font="sans serif"\n\
-[client]\n\
-toolbarMode = "viewer"\n\
-showSidebarNavigation = false\n\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "
+[theme]\n
+primaryColor=\"#CA1551\"\n
+backgroundColor=\"#00002C\"\n
+secondaryBackgroundColor=\"#011152\"\n
+textColor=\"#FFFFFF\"\n
+font=\"sans serif\"\n
+[client]\n
+toolbarMode = \"viewer\"\n
+showSidebarNavigation = false\n
+[server]\n
+headless = true\n
+port = $PORT\n
+enableCORS = false\n
 " > ~/.streamlit/config.toml
