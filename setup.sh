a2df10d3b36b670cc03d17f6163587085020f3f6
Arian Gohari <a.gohari@startmunich.de>
2024-04-22 00:44:51 +0200

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 mkdir -p ~/.streamlit/
 echo "
+[theme]\n
+primaryColor=\"#CA1551\"\n
+backgroundColor=\"#FFFFFF\"\n
+textColor=\"#011152\"\n
+font=\"sans serif\"\n
+[client]\n
+toolbarMode = \"minimal\"\n
+showSidebarNavigation = false\n
 [server]\n
 headless = true\n
 port = $PORT\n
