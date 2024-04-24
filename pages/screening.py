bb12e7523df6e2f92366b5d2e18934b30dc73d40
Arian Gohari <a.gohari@startmunich.de>
2024-04-24 15:20:16 +0200

    small design changes

--- pages/screening.py
+++ pages/screening.py
@@ -23,9 +23,10 @@ def load_dataframe(record_list):
     return df.drop(columns=["Index", "Respondant ID", "Submitted at"])
 
 
+
 # Render page
-def render_page(submission_id, screener_nr, completed, evaluation, application, questions, links):
-    with st.container(border=True):
+def render_page(user, submission_id, screener_nr, completed, evaluation, application, questions, links):
+    with st.container(border=False):
         st.header(f'{"✅" if completed else "📄"} Application {submission_id}')
 
         for i in range(len(questions)):
@@ -50,8 +51,10 @@ def render_page(submission_id, screener_nr, completed, evaluation, application, 
 
                 add_vertical_space(2)
 
-    with st.form("Evaluation Form"):
-        st.subheader("Evaluation")
+    st.divider()
+
+    with st.form("Evaluation Form", border=False):
+        st.subheader("STARTie Evaluation")
 
         qualitative_value = evaluation[f"Evaluation {screener_nr} - Qualitative"]
         quantitative_value = evaluation[f"Evaluation {screener_nr} - Quantitative"]
@@ -68,11 +71,11 @@ def render_page(submission_id, screener_nr, completed, evaluation, application, 
         qualitative = st.text_area(
             "Qualitative evaluation",
             value=qualitative_value, height=160)
-        quantitative = st.slider("Rank this candidate on a scale from 1 (Clear No) to 5 (Clear Yes)", min_value=1, max_value=5, step=1, value=quantitative_int)
+        quantitative = st.slider("Rank this candidate on a scale from 1 to 5", min_value=1, max_value=5, step=1, value=quantitative_int)
         interview = st.selectbox("Should we interview this candidate?", interview_options,
                                  index=interview_index,
                                  )
-        notes = st.text_area("Additional Notes", value=notes_value, height=120)
+        notes = st.text_area("Additional notes", value=notes_value, height=120)
         complete = st.checkbox("Mark as complete", value=complete_bool)
 
         # Check if all prior information is right
@@ -96,8 +99,10 @@ def render_page(submission_id, screener_nr, completed, evaluation, application, 
                 st.error("Error submitting evaluation. Please try again.")
 
     if completed:
-        with st.container(border=True):
-            st.subheader("Further Information")
+        add_vertical_space(2)
+        st.divider()
+        with st.container(border=False):
+            st.subheader("Further information")
             st.write("The following discloses name, gender and/or university of the candidate.")
             st.markdown("""
                 <style>
@@ -156,7 +161,7 @@ def main():
         st.sidebar.selectbox("Select an application:", submission_ids,
                              key="selected_submission",
                              index=selected_submission_index,
-                             format_func=lambda x: format_submission_title(x, submissions)
+                             format_func=lambda x: format_submission_title(x, submissions),
                              )
 
         st.sidebar.button("Logout", on_click=auth.logout)
@@ -174,6 +179,7 @@ def main():
                 evaluation = evaluation_result[0]
                 application = application_result[0]
                 render_page(
+                    user=user,
                     submission_id=selected_submission,
                     screener_nr=submissions[selected_submission][0],
                     completed=submissions[selected_submission][1],
